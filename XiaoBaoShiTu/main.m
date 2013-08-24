f11b01743a3c788082534bb7f4bd2bb02e89b461
longanxiang_iMac <longanxiang_imac@longanxiang-iMactekiMacBook-Pro.local>
2013-08-24 14:15:23 +0800

    Initial Commit

---
+++ XiaoBaoShiTu/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  XiaoBaoShiTu
+//
+//  Created by longanxiang_iMac on 13-8-24.
+//  Copyright (c) 2013年 tm. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
